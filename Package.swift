9654e590af6aec17621f3dbd72bcd327e3004aa4
Andrew Heard <andrewheard@google.com>
2023-08-02 12:55:50 -0400

    Add `appCheckVisionOS` `Platform` extension

--- Package.swift
+++ Package.swift
@@ -29,11 +29,11 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/google/promises.git",
-      "2.1.0" ..< "3.0.0"
+      "2.3.1" ..< "3.0.0"
     ),
     .package(
       url: "https://github.com/google/GoogleUtilities.git",
-      "7.11.0" ..< "8.0.0"
+      "7.11.5" ..< "8.0.0"
     ),
   ],
   targets: [
@@ -50,10 +50,25 @@ let package = Package(
             linkerSettings: [
               .linkedFramework(
                 "DeviceCheck",
-                .when(platforms: [.iOS, .macCatalyst, .macOS, .tvOS])
+                .when(platforms: [.iOS, .macCatalyst, .macOS, .tvOS, .appCheckVisionOS])
               ),
             ]),
     // TODO(andrewheard): Add unit test targets after removing Firebase dependencies.
-  ],
-  cLanguageStandard: .c99
+  ]
 )
+
+extension Platform {
+  // Xcode dependent value for the visionOS platform. Namespaced with an "appCheck" prefix to
+  // prevent any API collisions (such issues should not arise as the manifest APIs should be
+  // confined to the `Package.swift`).
+  static var appCheckVisionOS: Self {
+    #if swift(>=5.9)
+      // For Xcode 15, return the available `visionOS` platform.
+      return .visionOS
+    #else
+      // For Xcode 14, return `iOS` as `visionOS` is unavailable. Since all targets support iOS,
+      // this acts as a no-op.
+      return .iOS
+    #endif // swift(>=5.9)
+  }
+}
