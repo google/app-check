d6b709f294fa7b47c643f39740536c4c61936032
Andrew Heard <andrewheard@google.com>
2023-09-06 17:03:58 -0400

    Set default log level in `load` instead of getter (#23)

--- AppCheckCore/Sources/Core/GACAppCheckLogger.m
+++ AppCheckCore/Sources/Core/GACAppCheckLogger.m
@@ -27,14 +27,12 @@ NS_ASSUME_NONNULL_BEGIN
 // Note: Declared as volatile to make getting and setting atomic.
 static volatile GACAppCheckLogLevel _logLevel;
 
++ (void)load {
+  // Set the default log level (warning).
+  _logLevel = GACAppCheckLogLevelWarning;
+}
+
 + (GACAppCheckLogLevel)logLevel {
-  static dispatch_once_t once;
-  dispatch_once(&once, ^{
-    if (!_logLevel) {
-      // Set the default log level (warning) if not yet set.
-      _logLevel = GACAppCheckLogLevelWarning;
-    }
-  });
   return _logLevel;
 }
 
