918c743fc269b9a8d33e58f396647d9aff9f9497
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2024-05-09 19:02:42 -0400

    [Infra] Fix flakes in SPM tests by extending timeout (#56)
    Signed-off-by: Nick Cooke <36927374+ncooke3@users.noreply.github.com>

--- AppCheckCore/Tests/Unit/Core/GACAppCheckBackoffWrapperTests.m
+++ AppCheckCore/Tests/Unit/Core/GACAppCheckBackoffWrapperTests.m
@@ -361,8 +361,8 @@
                                       errorHandler:self.errorHandler];
 
   // 1.5 Wait for operation to complete.
-  [self waitForExpectationsWithTimeout:0.5 handler:NULL];
-  XCTAssert(FBLWaitForPromisesWithTimeout(0.5));
+  [self waitForExpectationsWithTimeout:1.0 handler:NULL];
+  XCTAssert(FBLWaitForPromisesWithTimeout(1.0));
 
   // 1.6 Expect the promise to be rejected with a backoff error.
   XCTAssertTrue(operationWithBackoff.isRejected);
@@ -381,8 +381,8 @@
                                                          errorHandler:self.errorHandler];
 
   // 2.4 Wait for operation to complete.
-  [self waitForExpectationsWithTimeout:0.5 handler:NULL];
-  XCTAssert(FBLWaitForPromisesWithTimeout(0.5));
+  [self waitForExpectationsWithTimeout:1.0 handler:NULL];
+  XCTAssert(FBLWaitForPromisesWithTimeout(1.0));
 
   // 2.5 Expect the promise to be rejected with a backoff error.
   XCTAssertTrue(operationWithBackoff.isRejected);
