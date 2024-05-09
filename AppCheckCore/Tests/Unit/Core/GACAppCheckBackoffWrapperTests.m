2791391a3851f45753b5fe87fb9a9ffe2235a4c7
Nick Cooke <nickcooke@google.com>
2024-05-09 14:34:05 -0400

    Revert "Extend timeout"
    This reverts commit 89e2f7a2b776083952bee7b64a38a713b06f3a9b.

--- AppCheckCore/Tests/Unit/Core/GACAppCheckBackoffWrapperTests.m
+++ AppCheckCore/Tests/Unit/Core/GACAppCheckBackoffWrapperTests.m
@@ -194,8 +194,8 @@
                                       errorHandler:self.errorHandler];
 
   // 1.4. Wait for operation to complete.
-  [self waitForExpectationsWithTimeout:20.0 handler:NULL];
-  XCTAssert(FBLWaitForPromisesWithTimeout(20.0));
+  [self waitForExpectationsWithTimeout:0.5 handler:NULL];
+  XCTAssert(FBLWaitForPromisesWithTimeout(0.5));
 
   // 1.5. Expect the promise to be rejected with the operation error.
   XCTAssertEqualObjects(operationWithBackoff.error, self.operationResult);
@@ -228,8 +228,8 @@
                                                          errorHandler:self.errorHandler];
 
   // 3.4. Wait for operation to complete.
-  [self waitForExpectationsWithTimeout:20.0 handler:NULL];
-  XCTAssert(FBLWaitForPromisesWithTimeout(20.0));
+  [self waitForExpectationsWithTimeout:0.5 handler:NULL];
+  XCTAssert(FBLWaitForPromisesWithTimeout(0.5));
 
   // 3.5. Expect the promise to be rejected with the operation error.
   XCTAssertEqualObjects(operationWithBackoff.value, self.operationResult);
@@ -244,8 +244,8 @@
                                                          errorHandler:self.errorHandler];
 
   // 3.8. Wait for operation to complete.
-  [self waitForExpectationsWithTimeout:20.0 handler:NULL];
-  XCTAssert(FBLWaitForPromisesWithTimeout(20.0));
+  [self waitForExpectationsWithTimeout:0.5 handler:NULL];
+  XCTAssert(FBLWaitForPromisesWithTimeout(0.5));
 
   // 3.9. Expect the promise to be rejected with the operation error.
   XCTAssertEqualObjects(operationWithBackoff.error, self.operationResult);
