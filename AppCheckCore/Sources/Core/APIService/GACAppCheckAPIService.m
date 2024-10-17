f478203539847c82ad74e326500fe94dcd13be64
Nick Cooke <nickcooke@google.com>
2024-10-17 18:49:58 -0400

    [AppCheckCore] Skip local cache when making network requests

--- AppCheckCore/Sources/Core/APIService/GACAppCheckAPIService.m
+++ AppCheckCore/Sources/Core/APIService/GACAppCheckAPIService.m
@@ -91,6 +91,7 @@ static NSString *const kDefaultBaseURL = @"https://firebaseappcheck.googleapis.c
              __block NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:requestURL];
              request.HTTPMethod = HTTPMethod;
              request.HTTPBody = body;
+             request.cachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
 
              if (self.APIKey) {
                [request setValue:self.APIKey forHTTPHeaderField:kAPIKeyHeaderKey];
