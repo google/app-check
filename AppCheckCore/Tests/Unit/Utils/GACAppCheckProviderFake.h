3c6d658bbb174dfaa06caa47c0391ab5c44df70f
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2026-07-17 13:35:53 -0400

    refactor: migrate GACAppCheckTests off OCMock (Milestone 1) (#107)

---
+++ AppCheckCore/Tests/Unit/Utils/GACAppCheckProviderFake.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+#import "AppCheckCore/Sources/Public/AppCheckCore/GACAppCheckProvider.h"
+#import "AppCheckCore/Sources/Public/AppCheckCore/GACAppCheckToken.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GACAppCheckProviderFake : NSObject <GACAppCheckProvider>
+
+@property(atomic, nullable) GACAppCheckToken *tokenToReturn;
+@property(atomic, nullable) NSError *errorToReturn;
+@property(nonatomic) NSInteger getTokenCallCount;
+
+@property(atomic, nullable) GACAppCheckToken *limitedUseTokenToReturn;
+@property(atomic, nullable) NSError *limitedUseErrorToReturn;
+@property(nonatomic) NSInteger getLimitedUseTokenCallCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AppCheckCore/Tests/Unit/Utils/GACAppCheckProviderFake.m
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "AppCheckCore/Tests/Unit/Utils/GACAppCheckProviderFake.h"
+
+@implementation GACAppCheckProviderFake
+
+@synthesize getTokenCallCount = _getTokenCallCount;
+@synthesize getLimitedUseTokenCallCount = _getLimitedUseTokenCallCount;
+
+- (NSInteger)getTokenCallCount {
+  @synchronized(self) {
+    return _getTokenCallCount;
+  }
+}
+
+- (void)setGetTokenCallCount:(NSInteger)getTokenCallCount {
+  @synchronized(self) {
+    _getTokenCallCount = getTokenCallCount;
+  }
+}
+
+- (NSInteger)getLimitedUseTokenCallCount {
+  @synchronized(self) {
+    return _getLimitedUseTokenCallCount;
+  }
+}
+
+- (void)setGetLimitedUseTokenCallCount:(NSInteger)getLimitedUseTokenCallCount {
+  @synchronized(self) {
+    _getLimitedUseTokenCallCount = getLimitedUseTokenCallCount;
+  }
+}
+
+- (void)getTokenWithCompletion:(void (^)(GACAppCheckToken *_Nullable token,
+                                         NSError *_Nullable error))handler {
+  GACAppCheckToken *token;
+  NSError *error;
+  @synchronized(self) {
+    _getTokenCallCount++;
+    token = self.tokenToReturn;
+    error = self.errorToReturn;
+  }
+  if (handler) {
+    handler(token, error);
+  }
+}
+
+- (void)getLimitedUseTokenWithCompletion:(void (^)(GACAppCheckToken *_Nullable token,
+                                                   NSError *_Nullable error))handler {
+  GACAppCheckToken *token;
+  NSError *error;
+  @synchronized(self) {
+    _getLimitedUseTokenCallCount++;
+    token = self.limitedUseTokenToReturn;
+    error = self.limitedUseErrorToReturn;
+  }
+  if (handler) {
+    handler(token, error);
+  }
+}
+
+@end
